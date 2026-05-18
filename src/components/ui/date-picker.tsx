b09585e16f95b0450fb21fd74880a8b73d77b323
Polaris <101608916+snap-star@users.noreply.github.com>
2026-05-18 02:41:32 +0000

    chore (deps): update package shadcn ui

--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -43,7 +43,7 @@ export function DatePicker() {
           )}
         >
           {/* <CalendarIcon className="mr-2 h-4 w-4" /> */}
-          {date ? format(date, 'dd/MM/yyyy HH:mm') + ' WIB' : <span>Pilih Tanggal & Waktu</span>}
+          {date ? `${format(date, 'dd/MM/yyyy HH:mm')} WIB` : <span>Pilih Tanggal & Waktu</span>}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">
